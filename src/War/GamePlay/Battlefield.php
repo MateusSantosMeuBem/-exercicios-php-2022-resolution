29f234dadd32724a2b3e6067c82e47e0be55502a
Mateus Santos <a.mateussm1.5@gmail.com>
2022-05-07 20:13:30 -0300

    :books: | sort and rolling the dices

--- src/War/GamePlay/Battlefield.php
+++ src/War/GamePlay/Battlefield.php
@@ -10,7 +10,18 @@ use Galoa\ExerciciosPhp2022\War\GamePlay\Country\CountryInterface;
 class Battlefield implements BattlefieldInterface {
 
     public function rollDice(CountryInterface $country, bool $isAtacking): array{
-        return array();
+
+        $dice_times = $country->getNumberOfTroops();
+
+        // This will generate an array with $dice_times random numbers from 1 to 6:
+        $player_dices = array_map(function () {
+            return rand(1, 6);
+        }, array_fill(0, $dice_times, null));
+
+        // Sorte from the biggest to the smallest one
+        rsort($player_dices);
+
+        return $player_dices;
     }
     
     public function computeBattle(CountryInterface $attackingCountry, array $attackingDice, CountryInterface $defendingCountry, array $defendingDice): void{
