fd29a8042180672850d8dbaa1d4e6ea4c48d6b18
Mateus Santos <a.mateussm1.5@gmail.com>
2022-05-09 17:01:50 -0300

    :sparkles: | Refactoring: Battlefield

--- src/War/GamePlay/Battlefield.php
+++ src/War/GamePlay/Battlefield.php
@@ -6,40 +6,75 @@ use Galoa\ExerciciosPhp2022\War\GamePlay\Country\CountryInterface;
 
 /**
  * A manager that will roll the dice and compute the winners of a battle.
+ * 
  */
 class Battlefield implements BattlefieldInterface {
 
     public function rollDice(CountryInterface $country, bool $isAtacking): array{
 
+        /**
+         * The number of this country will roll the dice.
+         * 
+         * @var int
+         * 
+        */        
         $dice_times = $country->getNumberOfTroops();
 
-        // This will generate an array with $dice_times if it's a defender, $dice_times - 1 if it's a attacker random numbers from 1 to 6:
+        /**
+         * This will generate an array with $dice_times if it's a defender,
+         * $dice_times - 1 if it's a attacker random numbers from 1 to 6.
+         * 
+         * @var array
+         * 
+         */
         $player_dices = array_map(function () {
             return rand(1, 6);
         }, array_fill(0, !$isAtacking ? $dice_times : $dice_times - 1, null));
 
-        // Sorte from the biggest to the smallest one
+        /**
+         * Sort from the biggest to the smallest one
+         * 
+         */
         rsort($player_dices);
 
         return $player_dices;
     }
     
     public function computeBattle(CountryInterface $attackingCountry, array $attackingDice, CountryInterface $defendingCountry, array $defendingDice): void{
-        
+        /**
+         * The each side number of troops.
+         * 
+         * @var int
+         * @var int
+         * 
+         */
         $attackingCountryNumber0fTroops = $attackingCountry->getNumberOfTroops();
         $defendingCountryNumber0fTroops = $defendingCountry->getNumberOfTroops();
-
+        /**
+         * Check which country has less troops
+         * 
+         */
         if($attackingCountryNumber0fTroops <= $defendingCountryNumber0fTroops){
             foreach($attackingDice as $index => $attackDice){
                 $defendDice = $defendingDice[$index];
-                // Kill loser troop
-                $attackDice > $defendDice ? $defendingCountry->killTroops(1) : $attackingCountry->killTroops(1);
+                /**
+                 * Kill loser troop
+                 * 
+                 */
+                $attackDice > $defendDice ?
+                $defendingCountry->killTroops(1) :
+                $attackingCountry->killTroops(1);
             }
         }else{
             foreach($defendingDice as $index => $defendDice){
                 $attackDice = $attackingDice[$index];
-                // Kill loser troop
-                $attackDice > $defendDice ? $defendingCountry->killTroops(1) : $attackingCountry->killTroops(1);
+                /**
+                 * Kill loser troop
+                 * 
+                 */
+                $attackDice > $defendDice ?
+                $defendingCountry->killTroops(1) :
+                $attackingCountry->killTroops(1);
             }
         }
 
