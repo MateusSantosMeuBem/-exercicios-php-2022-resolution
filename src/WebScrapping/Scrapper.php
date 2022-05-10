dd70a226853ff899bf229dbc8120d17f9820e42d
Mateus Santos <a.mateussm1.5@gmail.com>
2022-05-10 16:40:46 -0300

    :wrench: | Catching all necessary DOM information

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -18,12 +18,41 @@ class Scrapper {
     $xpath = new DOMXPath($dom);
     $elements = $xpath->query('//*[@class="col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4"]/a');
 
-    print_r($elements);
+    // print_r($elements);
     foreach($elements as $ancors){
-      foreach($ancors->childNodes as $counter => $subAncors){
-        print_r("$counter - " . $subAncors->nodeValue. "\n");
+      
+      $ancorsChildren = $ancors->childNodes;
+
+      $title = $ancorsChildren[0]->textContent;
+      $dirtyAuthors = $ancorsChildren[1];
+      $footer = $ancorsChildren[2];
+      $type = $footer->childNodes[0]->textContent;
+      $id = $footer->childNodes[1]->childNodes[1]->textContent;
+
+      $authors = array();
+
+      /**
+       * Iterates into each author, checks if it's a valid author,
+       * so, then, stores them in @var $authors[]
+       * 
+       */
+      foreach($dirtyAuthors->childNodes as $author){
+        if($author->nodeName == 'span'){
+          /**
+           * If the last character of the name is ;, then remove it
+           * 
+           */
+          if($author->textContent[strlen($author->textContent) - 1] == ';'){
+            $authorName = substr($author->textContent, 0, strlen($author->textContent) - 1);
+          }else{
+            $authorName = $author->textContent;
+          }
+
+          $authorInstitution = $author->attributes->getNamedItem('title')->nodeValue;
+
+          $authors[$authorName] = $authorInstitution;
+        }
       }
-      print "\n\n";
     }
   }
 
