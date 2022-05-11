e67d0951ef3cf36eefb1929bddfeef13ea624241
Mateus Santos <a.mateussm1.5@gmail.com>
2022-05-10 23:47:20 -0300

    :sparkles: | Refactoring: Scrapper

--- src/WebScrapping/Scrapper.php
+++ src/WebScrapping/Scrapper.php
@@ -12,47 +12,94 @@ class Scrapper implements ScrapperInterface{
 
   /**
    * Loads paper information from the HTML and creates a XLSX file.
+   * 
+   * @return mixed
+   * @var array $headerKeys
+   * All the keys for the xlsx docuement.
+   * @var array $data
+   * An array with the document rows.
+   * 
    */
   public function scrap(\DOMDocument $dom): mixed {
     
     $xpath = new DOMXPath($dom);
+    /**
+     * Element with all the aancors elements.
+     */
     $elements = $xpath->query('//*[@class="col-sm-12 col-md-8 col-lg-8 col-md-pull-4 col-lg-pull-4"]/a');
 
+    /**
+     * Array with non-repeted header keys.
+     */
     $tableKeys = [
       'ID',
       'Title',
       'Type'
     ];
 
+    /**
+     * @var array
+     *  Array to catch all the datas for the current row.
+     */
     $rowArray = array();
+    /**
+     * @var array
+     *  Array with all the body rows.
+     */
     $rows = array();
+    /**
+     * @var int
+     *  Variable to store the largest number of authors in a ancor item.
+     */
     $largestNumberOfAuthors = 0;
 
     foreach($elements as $ancor){
       
+      /**
+       * @var mixed
+       *  Object storing all the children from the current ancor item.
+       */
       $ancorChildren = $ancor->childNodes;
-
+      /**
+       * @var string
+       *  Current ancor item title.
+       */
       $title = $ancorChildren[0]->textContent;
+      /**
+       * @var mixed
+       * Object storing all the authores and some empty texts of the
+       * current ancor item.
+       */
       $dirtyAuthors = $ancorChildren[1];
+      /**
+       * Object storing children nodes with type and id of the
+       * current ancor item.
+       */
       $footer = $ancorChildren[2];
+      /**
+       * @var string
+       *  Current ancor item type.
+       */
       $type = $footer->childNodes[0]->textContent;
+      /**
+       * @var string
+       *  Current ancor item footer.
+       */
       $id = $footer->childNodes[1]->childNodes[1]->textContent;
 
 
-      /**
-       * Addint to rowArray
-       * 
-       */
       $rowArray[] = $id;
       $rowArray[] = $title;
       $rowArray[] = $type;
 
-      $authors = array();
-
       /**
        * Iterates into each author, checks if it's a valid author,
-       * so, then, stores them in @var $authors[]
+       * so, then, stores them in @var $rowArray[]
        * 
+       */
+      /**
+       * @var int
+       *  Current number of authors of this ancor item.
        */
       $counter = 0;
       foreach($dirtyAuthors->childNodes as $author){
@@ -67,6 +114,10 @@ class Scrapper implements ScrapperInterface{
             $authorName = $author->textContent;
           }
 
+          /**
+           * @var string
+           *  Current author institution.
+           */
           $authorInstitution = $author->attributes->getNamedItem('title')->nodeValue;
 
           $rowArray[] = $authorName;
@@ -78,10 +129,25 @@ class Scrapper implements ScrapperInterface{
           $largestNumberOfAuthors = $counter;
         }
       }
+
+      /**
+       * @var WriterEntityFactory
+       *  Current row object.
+       */
       $row = WriterEntityFactory::createRowFromArray($rowArray);
+      /**
+       * @var mixed
+       * Stores all the body row objects.
+       */
       $rows[] = $row;
+      /**
+       * Gets it empty.
+       */
       $rowArray = array();
     }
+    /**
+     * Finishes to create the array with all the header keys.
+     */
     for($authorNumber = 1; $authorNumber <= $largestNumberOfAuthors; $authorNumber++){
       $tableKeys[] = "Author $authorNumber";
       $tableKeys[] = "Author $authorNumber Institution";
